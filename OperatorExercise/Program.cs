79ab461d97da7b74737e345900c304ad06d295e4
justin long <justinxlong93@gmail.com>
2021-10-22 11:28:27 -0500

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int sum;
+
+            sum = a + b;
+
+            Console.WriteLine(sum);
+
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4);
+
+            Console.WriteLine(a);
+        }
+    }
+}
